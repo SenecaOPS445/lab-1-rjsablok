97a3efc7b3690ca52ecf7817be195457ef1b7a30
ram sablok <rjsablok@myseneca.ca>
2025-01-13 18:26:11 -0500

    added

---
+++ lab1a.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is a comment. Comments are ignored by Python.
+
+print('Hello world')
+
