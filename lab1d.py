d1abc3d8d2e80da9555cd5abcf7b6e5133387751
ram sablok <rjsablok@myseneca.ca>
2025-01-13 18:28:34 -0500

    added

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program performs simple math operations and prints the result'''
+
+# Variables
+x = 10
+y = 2
+z = 5
+
+# Perform the operation and print the result
+print(f"{x} + {y} * {z} = {x + y * z}")
